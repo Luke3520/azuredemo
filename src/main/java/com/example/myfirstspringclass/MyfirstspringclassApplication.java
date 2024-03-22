3f6765ded0546a6916dd28a36815efb637d07599
Luke3520 <luch0002@stud.kea.dk>
2024-03-22 14:08:27 +0100

    dat23a

---
+++ src/main/java/com/example/myfirstspringclass/MyfirstspringclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstspringclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myfirstspringclass/MyfirstspringclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstspringclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
