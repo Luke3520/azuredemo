3375d8aefc4b8e37694bccc17ebccf4a81996486
Luke3520 <luch0002@stud.kea.dk>
2024-03-22 14:16:20 +0100

    Fun fun

--- src/main/java/com/example/myfirstspringclass/MyfirstspringclassApplication.java
+++ src/main/java/com/example/myfirstspringclass/MyfirstspringclassApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyfirstspringclassApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyfirstspringclassApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstspringclassApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/myfirstspringclass/MyfirstspringclassApplicationTests.java
+++ src/test/java/com/example/myfirstspringclass/MyfirstspringclassApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyfirstspringclassApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
